7aa164caf925e913c80172334be9c05f4fd6220d
Nav479 <navaneeth.narayanan@fau.de>
2022-05-09 18:55:28 +0200

    Grid class final implementation

--- Grid.py
+++ Grid.py
@@ -3,25 +3,31 @@ import flat_panel_project_utils as utils
 
 
 class Grid:
-    o_pixel = np.array([0, 0])
 
-    def __init__(self, height, width, spacing):
+    def __init__(self, height, width, spacing, origin):
         self.height = height
         self.width = width
         self.spacing = spacing
+        self.origin = origin
         self.buffer = np.empty([height, width], dtype=float)
 
-    def get_origin(self):
-        o_wcs = Grid.o_pixel
-        o_wcs[0], o_wcs[1] = -0.5 * (self.width - 1) * self.spacing, -0.5 * (self.height - 1) * self.spacing
+    def origin_to_wcs(self):
+        o_wcs = self.origin
+        o_wcs[0], o_wcs[1] = -0.5 * (self.width - 1) * self.spacing[0], -0.5 * (self.height - 1) * self.spacing[1]
         return o_wcs
+
+    def get_origin(self):
+        return self.origin
+
+    def set_origin(self, o_x, o_y):
+        self.origin = o_x, o_y
 
     def get_size(self):
         size = np.array([self.width, self.height])
         return size
 
     def get_spacing(self):
-        spacing = np.array([self.spacing, self.spacing])
+        spacing = np.array([self.spacing[0], self.spacing[1]])
         return spacing
 
     def set_buffer(self, nd_array):
@@ -31,12 +37,12 @@ class Grid:
         return self.buffer
 
     def index_to_physical(self, i, j):
-        origin = Grid.get_origin(self)
-        return np.array([(i * self.spacing) + origin[0], (j * self.spacing) + origin[1]])
+        o = Grid.origin_to_wcs(self)
+        return np.array([(i * self.spacing[0]) + o[0], (j * self.spacing[1]) + o[1]])
 
     def physical_to_index(self, x, y):
-        return np.array([((x - Grid.o_pixel[0]) / self.spacing).astype('int64'),
-                         ((y - Grid.o_pixel[1]) / self.spacing).astype('int64')])
+        return np.array([((x - self.origin[0]) / self.spacing[0]),
+                         ((y - self.origin[1]) / self.spacing[1])])
 
     def set_at_index(self, i, j, val):
         if i >= Grid.get_size(self)[0] or j >= Grid.get_size(self)[1]:
@@ -47,7 +53,8 @@ class Grid:
     def get_at_index(self, i, j):
         return self.buffer[i][j]
 
-
-def get_at_physical(grid, x, y):
-    # not sure if correct implementation
-    return utils.interpolate(grid, x, y)
+    def get_at_physical(self, grid, x, y):
+        # not sure if correct implementation
+        index = Grid.physical_to_index(self, x, y)
+        i, j = index[0], index[1]
+        return utils.interpolate(grid, i, j)
--- test.py
+++ test.py
@@ -1,18 +1,28 @@
 import math
-
 import Grid as grid
 import flat_panel_project_utils as utils
-import matplotlib.pyplot as plt
 
+# **** TEST - 1 ****
+# ******* testing grid class on Shepp Logan phantom **********
 phantom = utils.shepp_logan(512)
-grid_phantom = grid.Grid(256,256,1)
+
+# note: constructor params are -> height, width, (s_x, s_y), (o_x, o_y)
+# spacing and origin are to be set as tuples
+grid_phantom = grid.Grid(len(phantom), len(phantom[0]), (1, 1), (0, 0))
 grid_phantom.set_buffer(phantom)
-utils.show(grid_phantom.get_buffer(), "Phantom ")
+utils.show(grid_phantom.get_buffer(), "Shepp Logan Phantom ")
 
-# val = grid_test.get_at_index(125, 340)
+print("Origin is set at:", grid_phantom.get_origin())
+print("Spacing values are:", grid_phantom.get_spacing())
+print("Grid size is:", grid_phantom.get_size())
+print("get at physical of phantom @",(150, 157),"is :", grid_phantom.get_at_physical(grid_phantom, 150, 157))
 
-# ******* testing grid class to draw a circle **********
-grid_test = grid.Grid(512, 512, 4)
+# *******  END **********
+
+
+# **** TEST - 2 ****
+# ******* Using grid class methods to draw a circle **********
+grid_test = grid.Grid(512, 512, (1, 1), (-255, -255))
 origin = grid_test.get_origin()
 
 radius = 100
@@ -23,17 +33,12 @@ for x in range(-radius, radius):
     for y in range(-radius, radius):
         if (math.pow(x, 2) + math.pow(y, 2)) <= math.pow(radius, 2):
             index = grid_test.physical_to_index(x, y)
-            i, j = index[0], index[1]
+            i, j = index[0].astype('int64'), index[1].astype('int64')
 
             if 0 <= i < width and 0 <= j < height:
                 grid_test.set_at_index(i, j, val)
-                print("values of Y", y)
 
-
-print("completed circle", grid_test.get_buffer())
 utils.show(grid_test.get_buffer(), "test")
 
-print("get at physical", grid.get_at_physical(grid_test, 200, 150))
+print("get at physical @",(20, 20),"is :", grid_test.get_at_physical(grid_test, 0, 100))
 # ******* circle test ended **********
-
-
