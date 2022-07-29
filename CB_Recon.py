96368d6645e04f2888a443e5e0555795463fafe9
Nav479 <navaneeth.narayanan@fau.de>
2022-07-29 13:35:34 +0200

    Calling CONRAD reconstruction pipeline through pyconrad - CB reconstruction

---
+++ CB_Recon.py
@@ -0,0 +1,4 @@
+import pyconrad as pyc
+pyc.setup_pyconrad(max_ram='8G')
+pyc.start_reconstruction_pipeline_gui()
+
--- testing_file.py
+++ testing_file.py
@@ -6,12 +6,12 @@ import time
 
 #### Test for unfiltered backprojection ######
 
-phantom = utils.shepp_logan(128)
+phantom = utils.shepp_logan(256)
 grid_phantom = grid.Grid(len(phantom), len(phantom[0]), (1, 1))
 grid_phantom.set_buffer(phantom)
 utils.show(grid_phantom.get_buffer(), " Phantom ")
 
-sinogram = recon.create_sinogram(grid_phantom.get_buffer(), 300, 1, 200, np.pi)
+sinogram = recon.create_sinogram(grid_phantom.get_buffer(), 200, 1, 400, np.pi)
 utils.show(np.rot90(sinogram.get_buffer(), k=1), "Sinogram ")
 
 filtered_sino = recon.ramp_filter(sinogram, 1)
@@ -20,7 +20,7 @@ utils.show(np.rot90(filtered_sino.get_buffer(), k=1), "Sinogram filtered")
 start_time = time.time()
 recon_image, library = recon.backproject(filtered_sino, grid_phantom.get_size()[0], grid_phantom.get_size()[1],grid_phantom.get_spacing())
 utils.show(recon_image, "FBP image ")
-print(" CPU backprojection run time (s) : %s" % (time.time() - start_time))
+print("CPU backprojection run time (s) : %s" % (time.time() - start_time))
 
 #utils.show(grid_phantom.get_buffer() - recon_image_unfiltered, "Error image (phantom v backprojection) ")
 #utils.show(grid_phantom.get_buffer() - np.rot90(library, k=-1), "Error image (Phantom v library)")
@@ -28,4 +28,4 @@ print(" CPU backprojection run time (s) : %s" % (time.time() - start_time))
 start_time = time.time()
 img_opencl = recon.backprojectOpenCL(filtered_sino, grid_phantom.get_size()[0], grid_phantom.get_size()[1], grid_phantom.get_spacing())
 utils.show(np.rot90(library, k=-1), "OpenCL recon image")
-print("  GPU accelerated backprojection run time(s) : %s" % (time.time() - start_time))
+print("GPU accelerated backprojection run time(s) : %s" % (time.time() - start_time))
